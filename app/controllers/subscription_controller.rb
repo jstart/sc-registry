7dc8515f9afa14958de44ef95247dbd5cc2d29c9
Chris Truman <cleetruman@gmail.com>
2015-01-17 23:07:50 -0800

    Subscription test

---
+++ app/controllers/subscription_controller.rb
@@ -0,0 +1,21 @@
+class SubscriptionController < ApplicationController
+
+def new
+end
+
+def create
+  # Amount in cents
+  @amount = 500
+
+  customer = Stripe::Customer.create(
+    :email => 'example@stripe.com',
+    :card  => params[:stripeToken],
+    :plan => "basic"
+  )
+
+rescue Stripe::CardError => e
+  flash[:error] = e.message
+  redirect_to charges_path
+end
+
+end
