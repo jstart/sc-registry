79edcdfc554e89c6b8a6f7bd075205e31f3ca0f8
Chris Truman <cleetruman@gmail.com>
2015-02-03 14:59:32 -0800

    Allow all car params

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -69,6 +69,6 @@ class CarsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def car_params
-      params.require(:car).permit(:display_name, :year)
+        params.require(:car).permit(:display_name, :year, :description, :make, :model, :color)
     end
 end
